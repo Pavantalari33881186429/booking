8b69320b35b24ebf800a95262abc7868830cd251
Pavan <talaripavankumar1997@gmail.com>
2024-08-02 19:24:08 +0530

    Added the cors handler

--- src/main/java/com/wipro/booking/controller/BookingController.java
+++ src/main/java/com/wipro/booking/controller/BookingController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.wipro.booking.vo.PaymentMessage;
 
 @RestController
 @RequestMapping("/booking")
+@CrossOrigin(origins = "*")
 public class BookingController {
     @Autowired
     private BookingService bookingService;

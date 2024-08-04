7c89b567618c1cc4843fee83e44e61de473e1e11
Pavan <talaripavankumar1997@gmail.com>
2024-08-04 14:58:37 +0530

    Added Jenkins file

--- src/main/java/com/wipro/booking/controller/BookingController.java
+++ src/main/java/com/wipro/booking/controller/BookingController.java
@@ -1,13 +1,11 @@
 package com.wipro.booking.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.Date;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,21 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wipro.booking.entity.Booking;
 import com.wipro.booking.fiegn.FlightClient;
-import com.wipro.booking.kafkaConfig.PaymentProducer;
 import com.wipro.booking.service.BookingService;
 import com.wipro.booking.vo.FlightVO;
 import com.wipro.booking.vo.PaymentMessage;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 
 @RestController
 @RequestMapping("/booking")
 @CrossOrigin(origins = "*")
 public class BookingController {
+	
+	private static final Logger log = LogManager.getLogger(BookingController.class);
     @Autowired
     private BookingService bookingService;
 
     @Autowired
     private FlightClient flightClient;
+    
     
    
 
@@ -48,7 +50,8 @@ public class BookingController {
     public Booking createBooking(@RequestBody Booking booking) {
         return bookingService.saveBooking(booking);
     }
-
+    
+    @CircuitBreaker(name = "searchFlights", fallbackMethod = "fallbackSearchFlights")
     @GetMapping("/search")
     public List<FlightVO> searchFlights(@RequestParam String source, @RequestParam String destination, @RequestParam String travelDate) {
         return flightClient.searchFlights(source, destination, travelDate);
@@ -80,5 +83,12 @@ public class BookingController {
     	
     	bookingService.paymentProcessing(message);
     	
+    }
+    
+    public List<FlightVO> fallbackSearchFlights(String source, String destination, String travelDate, Throwable throwable) {
+        
+		// Log the error and return a default response
+        log.error("Flight search failed: ", throwable);
+        return Collections.emptyList(); // Or provide a default response
     }
 }
